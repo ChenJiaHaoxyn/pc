7d4d5397309be829177d957cb16b63578f2d69b1
ChenJiaHaoxyn <49440621+ChenJiaHaoxyn@users.noreply.github.com>
2019-05-11 19:26:15 +0800

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,45 @@
+print("Hello Python world!")
+message = "Hello Python world!"
+print(message)
+message = "Hello Python world!"
+print(message)
+message = "Hello Python Crash Course world!"
+print(message)
+name = "ada lovelace"
+print(name.title())
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+import this
+a = input("input:")
+dir()   #  获得当前模块的属性列表
+a = 21
+b = 10
+c = 0
+c = a + b
+print(" c 的值为："+str(c))
+c = a - b
+print(" c 的值为："+str(c))
+c = a * b
+print(" c 的值为："+str(c))
+c = a / b
+print(" c 的值为："+str(c))
+c = a % b
+print(" c 的值为："+str(c))
+# 修改变量 a 、b 、c
+a = 2
+b = 3
+c = a ** b
+print(" c 的值为："+str(c))
+a = 10
+b = 5
+c = a // b
+print(" c 的值为："+str(c))
+
+
+
+
+
+
+
+
