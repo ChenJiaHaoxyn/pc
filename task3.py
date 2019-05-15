c0ba46839c5956b729b1df093161ceac9f6ab038
ChenJiaHaoxyn <49440621+ChenJiaHaoxyn@users.noreply.github.com>
2019-05-15 21:36:34 +0800

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,43 @@
+#a={key1:vaue1,key2:value2}
+a={}
+a={‘weather’:’sunny’,’color’:’blue’}
+print(a[‘weather’])
+
+print(a)
+a[‘x_position’]=2
+a[‘fruit’]=’apple’
+print(a)
+a[‘weather’]= frog
+print(a)
+del a[‘weather’]
+print(a)
+for k,v in a.items():
+print(“\nKey:”+k)
+print(“Value:”+v)
+for n in a.keys():
+print(n.title())
+a1={‘weather’:’sunny’,’color’:’blue’}
+a2={‘weather’:’sunny’,’color’:’green’}
+a3={‘weather’:’sunny’,’color’:’red’}
+a=[a1,a2,a3]
+for b in a:
+print(b)
+a={‘weather’:[’sunny’, ’frog’],
+’color’:[’blue’,’green’]}c.在字典中存储字典：
+a={‘x’:{‘weather’:’sunny’,‘color’:’blue’,},
+‘y’:{‘weather’:’frog’,‘color’:’blue’,},
+radiansdict.clear()
+#删除字典内所有元素
+radiansdict.copy()
+#返回一个字典的浅复制
+radiansdict.fromkeys()
+#创建一个新字典
+radiansdict.get(key, default=None)
+x={0,1,2,3,4,5}
+x.add(“s”)
+print(x)
+x.remove
+x.discard
+x.pop
+len(x)
+a in x
